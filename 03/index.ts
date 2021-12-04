3e783ffed1f7d6a2f1614297bdb6b4186e1e49c5
Lucas Gladding <l.gladding@vehikl.com>
2021-12-03 21:34:24 -0500

    Refactored 03.

--- 03/index.ts
+++ 03/index.ts
@@ -8,36 +8,28 @@ export function parse(name: string): Input {
     return contents.split('\n').map(item => item.split(''));
 }
 
-type Select = <T>(input: Array<T> | undefined) => T | undefined;
+type Select = <T>(input: T[] | undefined) => T | undefined;
 
-type Count = [string, number];
+function find(list: string[], select: Select) {
+    return _.flow([
+        _.countBy,
+        _.entries,
+        (item) => _.sortBy(item, _.last),
+        select,
+        _.head,
+    ])(list);
+}
 
 export function calculate1(inputs: Input, select: Select): string {
-    let output = '';
-    const transposed = _.zip(...inputs);
-    for (const item of transposed) {
-        const stats: Count[] = _.flow([
-            _.countBy,
-            _.entries,
-            (item) => _.sortBy(item, _.last),
-        ])(item);
-        output += select(stats)![0];
-    }
-    return output;
+    return _.zip(...inputs).map(item => {
+        return find(item as string[], select);
+    }).join('');
 }
 
 export function calculate2(input: Input, select: Select): string {
     let current = input;
-    const length = input[0].length;
-    for (let i = 0; i < length; i++) {
-        const numbers = current.map(item => item[i]);
-        const target = _.flow([
-            _.countBy,
-            _.entries,
-            (item) => _.sortBy(item, _.last),
-            select,
-            _.head,
-        ])(numbers);
+    for (let i = 0; i < input[0].length; i++) {
+        const target = find(current.map(item => item[i]), select);
         current = current.filter(item => item[i] === target);
     }
     return current[0].join('');
