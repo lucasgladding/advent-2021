635ba2c22074fabdbc1bd0cc0aca5a9d88335ead
Lucas Gladding <lucas@me.com>
2021-12-06 18:02:06 -0500

    Added solution for 06-2.

--- 06/index.spec.ts
+++ 06/index.spec.ts
@@ -1,19 +1,27 @@
-import {parse} from './index';
+import {loop2, parse1, parse2} from './index';
 
 describe('06', () => {
     it('runs part 1 example', () => {
-        const input = parse('06/example.txt');
-        for (let i = 0; i < 80; i++) {
-            input.run();
-        }
+        const input = parse1('06/example.txt');
+        input.loop(80);
         expect(input.size).toEqual(5934);
     });
 
     it('runs part 1', () => {
-        const input = parse('06/input.txt');
-        for (let i = 0; i < 80; i++) {
-            input.run();
-        }
+        const input = parse1('06/input.txt');
+        input.loop(80);
         expect(input.size).toEqual(350149);
+    });
+
+    it('runs part 2 example', () => {
+        const input = parse2('06/example.txt');
+        const count = loop2(input, 256);
+        expect(count).toEqual(26984457539);
+    });
+
+    it('runs part 2', () => {
+        const input = parse2('06/input.txt');
+        const count = loop2(input, 256);
+        expect(count).toEqual(1590327954513);
     });
 });
--- 06/index.ts
+++ 06/index.ts
@@ -1,9 +1,26 @@
+import _ from 'lodash';
 import {read} from '../helpers';
 
-export function parse(name: string): School {
+export function parse1(name: string): School {
     const contents = read(name);
     const items = contents.split(',').map(text => parseInt(text)).map(timer => new Lanternfish(timer));
     return new School(items);
+}
+
+export function parse2(name: string): number[] {
+    const contents = read(name);
+    const items = _.countBy(contents.split(',').map(text => parseInt(text)));
+    return [
+        items[0] || 0,
+        items[1] || 0,
+        items[2] || 0,
+        items[3] || 0,
+        items[4] || 0,
+        items[5] || 0,
+        items[6] || 0,
+        items[7] || 0,
+        items[8] || 0,
+    ];
 }
 
 export class Lanternfish {
@@ -29,14 +46,33 @@ export class School {
         return this.items.length;
     }
 
-    run() {
-        const created: Lanternfish[] = [];
-        this.items.forEach((item) => {
-            if (item.canCreate) {
-                created.push(new Lanternfish());
+    loop(times: number) {
+        for (let i = 0; i < times; i++) {
+            const created: Lanternfish[] = [];
+            for (const item of this.items) {
+                if (item.canCreate)
+                    created.push(new Lanternfish());
+                item.run();
             }
-            item.run();
-        });
-        this.items.push(...created);
+            this.items.push(...created);
+        }
     }
+}
+
+export function loop2(input: number[], times: number): number {
+    let current = input;
+    for (let i = 0; i < times; i++) {
+        current = [
+            current[1] || 0,
+            current[2] || 0,
+            current[3] || 0,
+            current[4] || 0,
+            current[5] || 0,
+            current[6] || 0,
+            (current[7] + current[0]) || 0,
+            current[8] || 0,
+            current[0] || 0,
+        ];
+    }
+    return _.sum(current);
 }
