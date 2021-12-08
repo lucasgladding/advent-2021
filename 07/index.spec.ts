65a3f15984f1a2de8870174f1ab51233b031376c
Lucas Gladding <lucas@me.com>
2021-12-07 19:53:30 -0500

    Refactored 07.

--- 07/index.spec.ts
+++ 07/index.spec.ts
@@ -1,27 +1,27 @@
-import {parse, run1, run2} from './index';
+import {cost1, cost2, parse, run} from './index';
 
 describe('07', () => {
     it('runs part 1 example', () => {
         const input = parse('07/example.txt');
-        const target = run1(input);
+        const target = run(input, cost1);
         expect(target).toEqual(37);
     });
 
     it('runs part 1', () => {
         const input = parse('07/input.txt');
-        const target = run1(input);
+        const target = run(input, cost1);
         expect(target).toEqual(336701);
     });
 
     it('runs part 2 example', () => {
         const input = parse('07/example.txt');
-        const target = run2(input);
+        const target = run(input, cost2);
         expect(target).toEqual(168);
     });
 
     it('runs part 2', () => {
         const input = parse('07/input.txt');
-        const target = run2(input);
+        const target = run(input, cost2);
         expect(target).toEqual(95167302);
     });
 });
--- 07/index.ts
+++ 07/index.ts
@@ -8,7 +8,7 @@ export function parse(name: string): number[] {
 
 type Cost = (position: number, target: number) => number;
 
-function run(input: number[], cost: Cost): number {
+export function run(input: number[], cost: Cost): number {
     const max = _.max(input);
     const costs = Array(max).fill(undefined).map((item, index) => {
         const distances = input.map(position => cost(position, index));
@@ -17,19 +17,11 @@ function run(input: number[], cost: Cost): number {
     return _.min(costs) ?? 0;
 }
 
-function cost1(position: number, target: number): number {
+export function cost1(position: number, target: number): number {
     return Math.abs(position - target);
 }
 
-function cost2(position: number, target: number): number {
+export function cost2(position: number, target: number): number {
     const distance = Math.abs(position - target);
     return (distance / 2) * (2 + (distance - 1));
-}
-
-export function run1(input: number[]): number {
-    return run(input, cost1);
-}
-
-export function run2(input: number[]): number {
-    return run(input, cost2);
 }
