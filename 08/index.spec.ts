943fb5e6d787bb84b5374815f58d2fe56e96e397
Lucas Gladding <lucas@me.com>
2021-12-08 19:42:01 -0500

    WIP

--- 08/index.spec.ts
+++ 08/index.spec.ts
@@ -1,15 +1,21 @@
-import {count1, parse} from './index';
+import {count, decode, parse} from './index';
 
 describe('08', () => {
     it('counts part 1 example', () => {
         const input = parse('08/example.txt');
-        const output = count1(input);
+        const output = count(input);
         expect(output).toEqual(26);
     });
 
     it('counts part 1', () => {
         const input = parse('08/input.txt');
-        const output = count1(input);
+        const output = count(input);
         expect(output).toEqual(512);
+    });
+
+    it('decodes part 2 example', () => {
+        const input = parse('08/example.txt');
+        const output = decode(input[0]);
+        expect(output).toEqual(5353);
     });
 });
--- 08/index.ts
+++ 08/index.ts
@@ -11,6 +11,7 @@ export function parse(name: string): Input {
 }
 
 const segments = {
+    0: 6,
     1: 2,
     2: 5,
     3: 5,
@@ -22,7 +23,7 @@ const segments = {
     9: 6,
 };
 
-export function count1(input: Input): number {
+export function count(input: Input): number {
     const counts = _.flow([
         (item) => _.map(item, _.last),
         _.flatten,
@@ -30,4 +31,51 @@ export function count1(input: Input): number {
         _.countBy,
     ])(input);
     return counts[segments[1]] + counts[segments[4]] + counts[segments[7]] + counts[segments[8]];
+}
+
+type Schema = Record<number, string>;
+
+function length(input: string[], target: number): string {
+    return input.find(item => item.length === target)!
+}
+
+function intersects(subject: string, target: string) {
+    return target.split('').every(char => {
+        return subject.includes(char);
+    });
+}
+
+function text(input: string[], target: string): string[] {
+    return input.filter(subject => {
+        return intersects(subject, target);
+    });
+}
+
+function unused(input: string[], schema: Schema): string[] {
+    return input.filter(item => !Object.values(schema).includes(item))
+}
+
+export function decode(parts: string[][]): number {
+    const [input] = parts;
+    const groups = _.groupBy(input, (item) => item.length);
+
+    const schema: Schema = {};
+    schema[1] = length(input, segments[1]);
+    schema[4] = length(input, segments[4]);
+    schema[7] = length(input, segments[7]);
+    schema[8] = length(input, segments[8]);
+
+    schema[3] = text(groups[5], schema[7])[0];
+    schema[9] = text(groups[6], schema[3])[0];
+
+    schema[0] = text(unused(input, schema), schema[7])[0];
+    schema[6] = unused(groups[6], schema)[0];
+
+    // schema[2] = undefined;
+    // schema[5] = undefined;
+
+    // schema[0] = undefined;
+    // schema[6] = undefined;
+
+    return 0;
 }
