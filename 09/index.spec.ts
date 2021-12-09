517ac98df34ae40ae24b873e0b51431a21b7677e
Lucas Gladding <lucas@me.com>
2021-12-09 08:26:45 -0500

    Implemented solution for 09-2.

--- 09/index.spec.ts
+++ 09/index.spec.ts
@@ -12,4 +12,16 @@ describe('09', () => {
         const heightmap = new Heightmap(input);
         expect(heightmap.level).toEqual(566);
     });
+
+    it('calculates part 2 example', () => {
+        const input = parse('09/example.txt');
+        const heightmap = new Heightmap(input);
+        expect(heightmap.product).toEqual(1134);
+    });
+
+    it('calculates part 2', () => {
+        const input = parse('09/input.txt');
+        const heightmap = new Heightmap(input);
+        expect(heightmap.product).toEqual(891684);
+    });
 });
--- 09/index.ts
+++ 09/index.ts
@@ -34,7 +34,7 @@ export class Heightmap {
         return lowest;
     }
 
-    adjacent(x: number, y: number): Point[] {
+    private adjacent(x: number, y: number): Point[] {
         return [
             this.point(x - 1, y),
             this.point(x + 1, y),
@@ -48,8 +48,59 @@ export class Heightmap {
             return undefined;
         return this.points[y][x];
     }
+
+    get product(): number {
+        const basins = _.sortBy(this.basins, basin => basin.size).reverse();
+        const subjects = basins.slice(0, 3);
+        return subjects.reduce((acc, item) => {
+            return acc * item.size;
+        }, 1);
+    }
+
+    private get basins(): Basin[] {
+        return this.lowest.map(point => this.basinAt(point));
+    }
+
+    private basinAt(point: Point): Basin {
+        const basin = new Basin([point]);
+        this.expand(basin, point);
+        return basin;
+    }
+
+    private expand(basin: Basin, from: Point) {
+        const adjacent = this.adjacent(from.x, from.y);
+        for (const point of adjacent) {
+            if (point.height === 9)
+                continue;
+            if (basin.hasPoint(point))
+                continue;
+            basin.append(point);
+            this.expand(basin, point);
+        }
+    }
 }
 
-class Point {
+export class Point {
     constructor(public x: number, public y: number, public height: number) { }
+
+    isEqual(to: Point): boolean {
+        return to.x === this.x && to.y === this.y;
+    }
+}
+
+export class Basin {
+    constructor(private points: Point[] = []) { }
+
+    append(point: Point) {
+        this.points.push(point);
+    }
+
+    hasPoint(point: Point): boolean {
+        const match = this.points.find(item => item.isEqual(point));
+        return match !== undefined;
+    }
+
+    get size(): number {
+        return this.points.length;
+    }
 }
