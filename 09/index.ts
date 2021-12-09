54bacbea6e328f9a264472a6f61896d91e3d4b69
Lucas Gladding <lucas@me.com>
2021-12-09 07:49:30 -0500

    Minor refactor.

--- 09/index.ts
+++ 09/index.ts
@@ -1,43 +1,51 @@
 import _ from 'lodash';
 import {read} from '../helpers';
 
-export function parse(name: string): number[][] {
+export function parse(name: string): Point[][] {
     const contents = read(name);
-    return contents.split('\n').map(item => item.split('').map(item => parseInt(item)));
+    return contents.split('\n').map((item, y) => {
+        return item.split('').map((height, x) => {
+            return new Point(x, y, parseInt(height));
+        })
+    });
 }
 
 export class Heightmap {
-    constructor(private points: number[][]) { }
+    constructor(private points: Point[][]) { }
 
     get level(): number {
-        return _.sum(this.lowest.map(item => 1 + item));
+        return _.sum(this.lowest.map(item => 1 + item.height));
     }
 
-    get lowest(): number[] {
+    get lowest(): Point[] {
         const lowest = [];
         const height = this.points.length;
         const width = this.points[0].length;
         for (let y = 0; y < height; y++) {
             for (let x = 0; x < width; x++) {
-                const height = this.height(x, y)!;
+                const current = this.point(x, y)!;
                 const adjacent = ([
-                    this.height(x - 1, y),
-                    this.height(x + 1, y),
-                    this.height(x, y - 1),
-                    this.height(x, y + 1),
-                ].filter(height => height !== undefined));
-                const lowestAdjacentHeight = _.min(adjacent) as number;
+                    this.point(x - 1, y),
+                    this.point(x + 1, y),
+                    this.point(x, y - 1),
+                    this.point(x, y + 1),
+                ].filter(height => height !== undefined)) as Point[];
+                const lowestAdjacentHeight = _.min(adjacent.map(point => point.height))!;
 
-                if (height < lowestAdjacentHeight)
-                    lowest.push(height);
+                if (current.height < lowestAdjacentHeight)
+                    lowest.push(current);
             }
         }
         return lowest;
     }
 
-    private height(x: number, y: number) {
+    private point(x: number, y: number) {
         if (!this.points[y])
             return undefined;
         return this.points[y][x];
     }
+}
+
+class Point {
+    constructor(public x: number, public y: number, public height: number) { }
 }
