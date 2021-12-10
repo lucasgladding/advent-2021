c17e945c414735b9f09489206678609a7864c8e7
Lucas Gladding <lucas@me.com>
2021-12-10 08:54:15 -0500

    Added solution for 10-2.

--- 10/index.spec.ts
+++ 10/index.spec.ts
@@ -1,15 +1,27 @@
-import {parse, score} from './index';
+import {parse, score1, score2} from './index';
 
 describe('10', () => {
     it('score part 1 example', () => {
         const input = parse('10/example.txt');
-        const output = score(input);
+        const output = score1(input);
         expect(output).toEqual(26397);
     });
 
     it('score part 1', () => {
         const input = parse('10/input.txt');
-        const output = score(input);
+        const output = score1(input);
         expect(output).toEqual(392421);
+    });
+
+    it('scores part 2 example', () => {
+        const input = parse('10/example.txt');
+        const output = score2(input);
+        expect(output).toEqual(288957);
+    });
+
+    it('scores part 2', () => {
+        const input = parse('10/input.txt');
+        const output = score2(input);
+        expect(output).toEqual(2769449099);
     });
 });
--- 10/index.ts
+++ 10/index.ts
@@ -23,6 +23,10 @@ class IllegalCharacterException {
     constructor(public expected: string, public found: string) { }
 }
 
+class IncompleteException {
+    constructor(public missing: string) { }
+}
+
 // If a chunk opens with (, it must close with ).
 // If a chunk opens with [, it must close with ].
 // If a chunk opens with {, it must close with }.
@@ -43,13 +47,17 @@ function scan(input: string) {
             }
         }
     });
+    if (stack.length > 0) {
+        const missing = stack.reverse().map(open => find(open)[1]).join('');
+        throw new IncompleteException(missing);
+    }
 }
 
 // ): 3 points.
 // ]: 57 points.
 // }: 1197 points.
 // >: 25137 points.
-export function score(input: string[]): number {
+export function score1(input: string[]): number {
     const invalid = [];
     for (const item of input) {
         try {
@@ -68,4 +76,41 @@ export function score(input: string[]): number {
     };
     const scores = invalid.map(item => points[item]);
     return _.sum(scores);
+}
+
+function mid(input: number[]): number {
+    const target = (input.length - 1) / 2;
+    return input[target];
+}
+
+// ): 1 point.
+// ]: 2 points.
+// }: 3 points.
+// >: 4 points.
+export function score2(input: string[]): number {
+    const incomplete = [];
+    for (const item of input) {
+        try {
+            scan(item);
+        } catch (exception) {
+            if (exception instanceof IncompleteException) {
+                incomplete.push(exception.missing);
+            }
+        }
+    }
+    const points: Record<string, number> = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4,
+    };
+    const scores = incomplete.map(item => {
+        return item.split('').reduce((acc, item) => {
+            return acc * 5 + points[item];
+        }, 0);
+    });
+    const sorted = scores.sort(function(a, b) {
+        return a - b;
+    });
+    return mid(sorted);
 }
