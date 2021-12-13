3b905e0e02efdf20554a41fd29802bfddbbf7487
Lucas Gladding <lucas@me.com>
2021-12-12 23:32:55 -0500

    WIP

--- 12/index.spec.ts
+++ 12/index.spec.ts
@@ -6,4 +6,16 @@ describe('12', () => {
         const graph = new Graph(input);
         expect(graph.paths.length).toEqual(10);
     });
+
+    it('finds part 1 example 2', () => {
+        const input = parse('12/example-2.txt');
+        const graph = new Graph(input);
+        expect(graph.paths.length).toEqual(19);
+    });
+
+    it('finds part 1 example 3', () => {
+        const input = parse('12/example-3.txt');
+        const graph = new Graph(input);
+        expect(graph.paths.length).toEqual(226);
+    });
 });
