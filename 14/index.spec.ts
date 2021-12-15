780664490a84dc69db499c49765d867fb6f0fef8
Lucas Gladding <lucas@me.com>
2021-12-14 22:23:22 -0500

    Added solution for 14-2.

--- 14/index.spec.ts
+++ 14/index.spec.ts
@@ -1,4 +1,4 @@
-import {parse, Template} from './index';
+import {parse, Template, Template2} from './index';
 
 describe('14', () => {
     it('runs part 1 example', () => {
@@ -15,10 +15,17 @@ describe('14', () => {
         expect(template.score).toEqual(3408);
     });
 
-    xit('runs part 2', () => {
+    it('runs part 2 example', () => {
+        const input = parse('14/example.txt');
+        let template = Template2.create(input[0]);
+        template = template.process(input[1], 10);
+        expect(template.score).toEqual(1588);
+    });
+
+    it('runs part 2', () => {
         const input = parse('14/input.txt');
-        let template = new Template(input[0]);
+        let template = Template2.create(input[0]);
         template = template.process(input[1], 40);
-        expect(template.score).toEqual(0);
+        expect(template.score).toEqual(3724343376942);
     });
 });
--- 14/index.ts
+++ 14/index.ts
@@ -17,8 +17,16 @@ export function parse(name: string): [string, InstructionSet] {
     return [base, instructionSet];
 }
 
+function getGroups(input: string): string[] {
+    const output: string[] = [];
+    for (let i = 1; i < input.length; i++) {
+        output.push(input[i - 1] + input[i]);
+    }
+    return output;
+}
+
 export class Template {
-    static groups(groups: string[]): Template {
+    static create(groups: string[]): Template {
         let input = groups[0][0];
         for (const group of groups) {
             input += group.slice(1);
@@ -29,18 +37,13 @@ export class Template {
     constructor(private input: string) { }
 
     private get groups(): string[] {
-        const output: string[] = [];
-        for (let i = 1; i < this.input.length; i++) {
-            output.push(this.input[i - 1] + this.input[i]);
-        }
-        return output;
+        return getGroups(this.input);
     }
 
     process(instructions: InstructionSet, steps: number): Template {
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         let current: Template = this;
         for (let i = 0; i < steps; i++) {
-            console.log('processing', i);
             current = current.insert(instructions);
         }
         return current;
@@ -54,7 +57,11 @@ export class Template {
             }
             return group;
         });
-        return Template.groups(groups);
+        return Template.create(groups);
+    }
+
+    get size(): number {
+        return this.input.length;
     }
 
     get score(): number {
@@ -62,5 +69,50 @@ export class Template {
         const most = _.maxBy(Object.entries(counts), item => item[1])![1];
         const least = _.minBy(Object.entries(counts), item => item[1])![1];
         return most - least;
+    }
+}
+
+export class Template2 {
+    static create(input: string): Template2 {
+        const groups = getGroups(input);
+        const stats = _.countBy(groups);
+        return new Template2(input[0], stats);
+    }
+
+    constructor(private first: string, private stats: Record<string, number>) { }
+
+    process(instructions: InstructionSet, steps: number): Template2 {
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        let current: Template2 = this;
+        for (let i = 0; i < steps; i++) {
+            current = current.insert(instructions);
+        }
+        return current;
+    }
+
+    insert(instructions: InstructionSet): Template2 {
+        const stats: Record<string, number> = { ...this.stats };
+        for (const [text, count] of Object.entries(this.stats)) {
+            const instruction = instructions[text];
+            if (instruction) {
+                const a = text[0] + instruction;
+                const b = instruction + text[1];
+                stats[text] = stats[text] - count;
+                stats[a] = (stats[a] ?? 0) + count;
+                stats[b] = (stats[b] ?? 0) + count;
+            }
+        }
+        return new Template2(this.first, stats);
+    }
+
+    get score(): number {
+        const stats = Object.entries(this.stats).reduce((acc, [text, count]) => {
+            const char = text[1];
+            acc[char] = (acc[char] ?? 0) + count;
+            return acc;
+        }, {} as Record<string, number>);
+        stats[this.first] = (stats[this.first] ?? 0) + 1;
+        const counts = Object.values(stats);
+        return _.max(counts)! - _.min(counts)!;
     }
 }
